d2103e1a868c9e94fc5689715368321409634131
Mark Scheuerell <scheuerl@uw.edu>
2021-12-09 06:11:00 -0800

    added data ingest and shrimp transform for analysis

--- R/04_model-fitting.R
+++ R/04_model-fitting.R
@@ -7,18 +7,33 @@
 library(here)
 library(readr)
 library(dplyr)
+library(tidyr)
 library(MARSS)
+
 
 ## data directories
 clean_data_dir <- here("data", "clean")
 
-## clean file name
-clean_file <- "shrimp_data_for_analysis.csv"
+## clean file names
+clean_file_shrimp <- "shrimp_data_for_analysis.csv"
+clean_file_oni <- "oni_data_for_analysis.csv"
 
 
 #### read data ####
 
-data_clean <- read_csv(file.path(clean_data_dir, clean_file))
+## cleaned shrimp data
+shrimp_data <- read_csv(here(clean_data_dir, clean_file_shrimp))
+
+## ONI data
+oni_data <- read_csv(here(clean_data_dir, clean_file_oni))
 
 
+#### transform data ####
+
+## shrimp data for MARSS
+shrimp_trans <- shrimp_data %>%
+  pivot_wider(names_from = genus, values_from = cpue) %>%
+  select(-year) %>%
+  log() %>%
+  t()
 
